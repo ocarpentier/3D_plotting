05471598a89fce95f01aab5475a7b061a9a7f8d6
oscar <oscar.carpentier@hotmail.com>
2020-03-12 17:27:04 +0100

    plotter class with vector and stream representations

--- 3D plot.py
+++ 3D plot.py
@@ -1,9 +1,183 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+# from mayavi.mlab import *
+#
+# class plotter:
+#     def __init__(self,data):
+#         self.data = data
 
+x = np.linspace(0,10,10)
+y = np.linspace(0,10,10)
+z = np.linspace(0,10,10)
+u = np.sin(x)
+v = np.cos(x)
+w = np.sin(x)
+matrix = np.zeros((len(x),6))
+for i in range(len(x)):
+    matrix[i][0],matrix[i][1],matrix[i][2],matrix[i][3],matrix[i][4],matrix[i][5] = x[i],y[i],z[i],u[i],v[i],w[i]
+print(matrix)
 
 class plotter:
-    def __init__(self,data):
-        self.data = data
+    """
+    input is a numpay array/matrix
+    """
+
+    def vectorplanex(self, x):
+        # choose an interval which is projected on chosen y coordinata
+        interval = 10.01
+        data = self
+
+        # select points for certain z
+        data = data[(data[:, 2] < x + interval) & (data[:, 2] > x - interval)]
+
+        # create 2D grid of the data point
+        X, Y = np.meshgrid(data[:, 1], data[:, 2])
+        U, V = np.meshgrid(data[:, 4], data[:, 5])
+
+        # colour based on the total speed
+        color = np.hypot(U, V)
+
+        ##plot data
+        fig1, ax1 = plt.subplots()
+        ax1.set_title('Arrows scale with plot width, not view')
+        ax1.quiver(X, Y, U, V, color, alpha=0.8)
+        ax1.set_aspect('equal')  # sets aspect ratio to 1:1
+        # ax1.xaxis.set_ticks([])         #removes bars on axis
+        # ax1.yaxis.set_ticks([])         # removes bars on axis
+
+        plt.show()
+
+    # ------------------------------------------------------------------------------
+    def streamsplanex(self, x):
+        # choose an interval which is projected on chosen y coordinata
+        interval = 10.01
+        data = self
+
+        # select points for certain z
+        data = data[(data[:, 2] < x + interval) & (data[:, 2] > x - interval)]
+
+        # create 2D grid of the data point
+        X, Y = np.meshgrid(data[:, 0], data[:, 2])
+        U, V = np.meshgrid(data[:, 3], data[:, 5])
+
+        # colour based on the total speed
+        color = np.hypot(U, V)
+
+        ##plot data
+        fig1, ax1 = plt.subplots()
+        ax1.set_title('Arrows scale with plot width, not view')
+        ax1.streamplot(X, Y, U, V, density=0.8)
+        ax1.set_aspect('equal')  # sets aspect ratio to 1:1
+        # ax1.xaxis.set_ticks([])         #removes bars on axis
+        # ax1.yaxis.set_ticks([])         # removes bars on axis
+
+        plt.show()
+        # -----------------------------------------
+    def vectorplaney(self, y):
+        # choose an interval which is projected on chosen y coordinata
+        interval = 10.01
+        data = self
+
+        # select points for certain z
+        data = data[(data[:, 2] < y + interval) & (data[:, 2] > y - interval)]
+
+        # create 2D grid of the data point
+        X, Y = np.meshgrid(data[:, 0], data[:, 2])
+        U, V = np.meshgrid(data[:, 3], data[:, 5])
+
+        # colour based on the total speed
+        color = np.hypot(U, V)
+
+        ##plot data
+        fig1, ax1 = plt.subplots()
+        ax1.set_title('Arrows scale with plot width, not view')
+        ax1.quiver(X, Y, U, V, color, alpha=0.8)
+        ax1.set_aspect('equal')  # sets aspect ratio to 1:1
+        # ax1.xaxis.set_ticks([])         #removes bars on axis
+        # ax1.yaxis.set_ticks([])         # removes bars on axis
+
+        plt.show()
+
+        # ------------------------------------------------------------------------------
+    def streamsplaney(self, y):
+        # choose an interval which is projected on chosen y coordinata
+        interval = 10.01
+        data = self
+
+        # select points for certain z
+        data = data[(data[:, 2] < y + interval) & (data[:, 2] > y - interval)]
+
+        # create 2D grid of the data point
+        X, Y = np.meshgrid(data[:, 0], data[:, 2])
+        U, V = np.meshgrid(data[:, 3], data[:, 5])
+
+        # colour based on the total speed
+        color = np.hypot(U, V)
+
+        ##plot data
+        fig1, ax1 = plt.subplots()
+        ax1.set_title('Arrows scale with plot width, not view')
+        ax1.streamplot(X, Y, U, V, density=0.8)
+        ax1.set_aspect('equal')  # sets aspect ratio to 1:1
+        # ax1.xaxis.set_ticks([])         #removes bars on axis
+        # ax1.yaxis.set_ticks([])         # removes bars on axis
+        # qk = ax1.quiverkey(Q, 0.9, 0.9, 2, r'$2 \frac{m}{s}$', labelpos='E',coordinates='figure')
+        plt.show()
+        # -----------------------------------------
+
+    def vectorplanez(self,z):
+        # choose an interval which is projected on chosen y coordinata
+        interval = 10.01
+        data = self
+
+        #select points for certain z
+        data = data[(data[:,2]< z+interval) & (data[:,2]>z-interval)]
+
+        #create 2D grid of the data point
+        X,Y = np.meshgrid(data[:,0],data[:,1])
+        U,V = np.meshgrid(data[:,3],data[:,4])
+
+        # colour based on the total speed
+        color = np.hypot(U,V)
+
+        ##plot data
+        fig1, ax1 = plt.subplots()
+        ax1.set_title('Arrows scale with plot width, not view')
+        ax1.quiver(X, Y, U, V, color,alpha=0.8)
+        ax1.set_aspect('equal')         #sets aspect ratio to 1:1
+        # ax1.xaxis.set_ticks([])         #removes bars on axis
+        # ax1.yaxis.set_ticks([])         # removes bars on axis
+
+        plt.show()
+    #------------------------------------------------------------------------------
+    def streamsplanez(self,z):
+        # choose an interval which is projected on chosen y coordinata
+        interval = 10.01
+        data = self
+
+        #select points for certain z
+        data = data[(data[:,2]< z+interval) & (data[:,2]>z-interval)]
+
+        #create 2D grid of the data point
+        X,Y = np.meshgrid(data[:,0],data[:,1])
+        U,V = np.meshgrid(data[:,3],data[:,4])
+
+        # colour based on the total speed
+        color = np.hypot(U,V)
+
+        ##plot data
+        fig1, ax1 = plt.subplots()
+        ax1.set_title('Arrows scale with plot width, not view')
+        ax1.streamplot(X,Y,U,V,density=0.8)
+        ax1.set_aspect('equal')         #sets aspect ratio to 1:1
+        # ax1.xaxis.set_ticks([])         #removes bars on axis
+        # ax1.yaxis.set_ticks([])         # removes bars on axis
+
+        plt.show()
+        #-----------------------------------------
+
+
+plotter.streamsplaney(matrix,1.111)
 
---
+++ copy 3D plot.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+# from mayavi.mlab import *
+#
+# class plotter:
+#     def __init__(self,data):
+#         self.data = data
+
+x = np.linspace(0,10,10)
+y = np.linspace(10,0,10)
+z = np.linspace(0,10,10)
+u = np.sin(x)
+v = np.cos(x)
+w = np.tan(x)
+matrix = np.zeros((len(x),6))
+for i in range(len(x)):
+    matrix[i][0],matrix[i][1],matrix[i][2],matrix[i][3],matrix[i][4],matrix[i][5] = x[i],y[i],z[i],u[i],v[i],w[i]
+
+class plotter:
+    """
+
+    """
+    interval = 0.01
+    def __init__(self):
+        self
+
+
+    def plotplane(self,z):
+        # put the array in dataframe
+        df = pd.DataFrame(self)
+
+
+        #select points for certain z
+        # df = df[(df[2] > z-self.interval) & (df[2]< z+self.interval)]
+        print(df[:,0])
+
+        ##plot data
+        fig1, ax1 = plt.subplots()
+        ax1.set_title('Arrows scale with plot width, not view')
+        Q = ax1.quiver(df[:,0], df[:,1], df[:,3], df[:,4], units='width')
+        qk = ax1.quiverkey(Q, 0.9, 0.9, 2, r'$2 \frac{m}{s}$', labelpos='E',coordinates='figure')
+        plt.show()
+
+matrix.plotter
+matrix.plotplane(0)
